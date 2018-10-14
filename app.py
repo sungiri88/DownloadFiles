414568a51c27b32eee5dc446fe5d46390b08afbc
umapathy <sungiri88@yahoo.com>
2018-10-14 10:49:29 +0100

    Updated code for regex.

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask,render_template,request,jsonify
-import requests,json
+from flask import Flask,render_template,request,jsonify,send_file
+import requests,json,glob
+from datetime import datetime
 app = Flask(__name__)
 
 # Variables
-artifactoryUrl='http://artifactory.in/artifactory/api' 
-username="username"
-password="password" 
+filesDir="/var/www/flask/DownloadFiles/files/"
 
 # Common Functions
 def getRequest(url,username,password):
@@ -18,24 +17,35 @@ def postRequest(url,payload,username,password):
 def main():
     return render_template('index.html')
 
-@app.route('/api/listDockerRepo/<string:repoName>', methods=['GET'])
-def listDockerRepo(repoName):
-	try:    
-		response=getRequest(artifactoryUrl+'/docker/'+repoName+'/v2/_catalog',username,password)	
-		print response.text
-		return json.dumps(response.text)
-	except Exception, e:
-		return(str(e)) 
+@app.route("/api/files",methods = ['POST'])
+def files():
+    content = request.get_json()
+    objDate = datetime.strptime(content['date'], '%m/%d/%Y')
+    year=datetime.strftime(objDate,'%Y')
+    day=datetime.strftime(objDate,'%d')
+    month=datetime.strftime(objDate,'%m')
+    if content['domain']:
+        regex=content['domain']+"_"+year+"_"+month+"_"+day
+    else:
+        regex=year+"_"+month+"_"+day    
+    print regex    
+    mylist = [f for f in glob.glob1(filesDir,"*"+regex+".zip")]
+    return json.dumps(mylist)
 
-@app.route('/api/search/aql', methods=['POST'])
-def searchAql():
-	try:		    
-		data=request.data		
-		response=postRequest(artifactoryUrl+'/search/aql',data,username,password)
-		print(response.text)
-		return jsonify(response.text)
-	except Exception, e:
-		return(str(e)) 		
-
+@app.route("/api/listFiles")
+def ListFiles():
+    try:
+	mylist = [f for f in glob.glob1(filesDir,"*.zip")]
+	return json.dumps(mylist)
+    except Exception as e:
+	print e	
+		
+@app.route("/api/downloadlogfile/<path>")
+def DownloadLogFile (path = None):
+    try:
+	print path
+        return send_file(filesDir+path, as_attachment=True)
+    except Exception as e:
+	print e
 if __name__ == "__main__":
-    app.run(debug=True,host="10.223.251.165",port="4000")
+    app.run(debug=True,host="192.168.33.10",port="4000")
--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
+    $( "#datepicker" ).datepicker();
+    var domain=['soa_server1','soa_server2']
+    $.each(domain,function(i,v){
+      $("#domain").append('<option>'+v+'</option>');
+    })
 // Inital functions to hide all views and load dashboard updates.
     loadDashboardUpdates();
     hideAllViews();
     showView("dashboard-view");
+    $( "#searchForm" ).submit(function( event ) {
+      var domain=$("#domain option:selected").text();
+      var selectedDate=$("#datepicker").val();
+      searchFiles(selectedDate,domain);
+     event.preventDefault();
+    });
 // Bind all Views with Side Navlinks
     $("[class$=-hyperlink]").click(function(e){
     e.preventDefault();
@@ -12,6 +23,27 @@ $(document).ready(function(){
     });
 });
 
+var searchFiles=function(inputDate,inputDomain){
+  var data={date:"",domain:"test"};
+  data.date=inputDate 
+  data.domain=inputDomain 
+  $.when(postJSON("/api/files",data)).done(function(result){
+    var fileHtml='<ul>';
+    if(result.length > 0)
+    {
+      $.each(result,function(i,v){
+        fileHtml+='<li><a href="/api/downloadlogfile/'+v+'">'+v+'</a></li>'
+        console.log(fileHtml)
+      })
+    }
+    else
+      fileHtml+='<li>No Records Found!</li>'
+    fileHtml+='</ul>';
+    console.log(fileHtml);          
+    $("#fileList").html(fileHtml);
+  })
+          
+}
 var loadDashboardUpdates = function(){
 
 
@@ -33,12 +65,40 @@ var loadTemplates = function () {
     
     }
 
+
+
+var postJSON = function(apiPath,inputdata){
+    var defer = new $.Deferred();
+    var data;
+    $.ajax({
+              method: "POST",
+              processData: "false",
+              dataType: 'json',
+              headers: {
+                "content-type": "application/json",
+                "cache-control": "no-cache"                
+              },
+              url: apiPath,
+              data: JSON.stringify(inputdata),
+              success: function (response) {
+                  data=response;
+                  console.log(data);
+                  defer.resolve(response);
+              },
+              error: function (i) {
+                console.log("error");
+                  console.log(i.statusText);
+              }
+          });
+    return defer;
+  }
+
 var getJSON = function(apiPath){
     var defer = new $.Deferred();
     var data;
     $.ajax({
               type: "GET",
-              url: "http://10.223.251.165:4000"+apiPath,
+              url: apiPath,
               success: function (response) {
                   data=response;
                   console.log(data);
